9956ef7c7c7be1910603bece260131b03d9ca5b1
taha <tahamiri02@gmail.com>
2023-09-04 09:06:23 +0330

    add postgres database.

--- library/settings.py
+++ library/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
